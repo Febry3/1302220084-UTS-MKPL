6c3bb5051f999a8cf8184fe5450aacade47c3954
Fehehehe <febryt22@gmail.com>
2025-04-18 23:25:16 +0700

    first commit

---
+++ taxcalculator_1302220084/TaxCalculator_1302220084.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taxcalculator_1302220084;
+
+/**
+ *
+ * @author Admin
+ */
+public class TaxCalculator_1302220084 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
