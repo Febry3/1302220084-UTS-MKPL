b941ae7c771c5a476def35cdebc1c1e91b95feae
Fehehehe <febryt22@gmail.com>
2025-04-19 00:54:45 +0700

    LongParameterList: menggabungkan beberapa parameter yang dapat digabungkan menjadi class yang dibuat sebelumnya sehingga parameter input berubah menjadi objek dari sebuah class [Constructor Employee, setSpouse, dan addChild]

--- taxcalculator_1302220084/TaxFunction.java
+++ taxcalculator_1302220084/TaxFunction.java
@@ -10,33 +10,32 @@ package com.mycompany.taxcalculator_1302220084;
 // * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
 // * 
 // */
-
 public class TaxFunction {
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-                    return 0;
-		}else {
-                    return tax;
-		}
-            
-			 
-	}
-	
+
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+
+        int tax = 0;
+
+        if (numberOfMonthWorking > 12) {
+            System.err.println("More than 12 month working per year");
+        }
+
+        if (numberOfChildren > 3) {
+            numberOfChildren = 3;
+        }
+
+        if (isMarried) {
+            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+        } else {
+            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+        }
+
+        if (tax < 0) {
+            return 0;
+        } else {
+            return tax;
+        }
+
+    }
+
 }
