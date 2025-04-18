b78d29d7ae74bb648e3deef8b666b4a68ec7a037
Fehehehe <febryt22@gmail.com>
2025-04-19 02:32:15 +0700

    DuplicateCode: mengabungkan beberapa perhitungan pajak yang berulang-ulang pada sebuah variabel sebagai penampung nilai secara sementara

--- taxcalculator_1302220084/TaxFunction.java
+++ taxcalculator_1302220084/TaxFunction.java
@@ -20,18 +20,16 @@ public class TaxFunction {
     private final static int NON_TAXABLE_ADDITION_FOR_NON_MARRIED = 54000000;
 
     public static int calculateTax(Employee employee) {
-        int tax = 0;
         int numberOfChildren = employee.getChildren().size() > 3 ? employee.getChildren().size() : MAX_CHILD_NOT_TAXED;
 
         if (employee.getMonthWorkingInYear() > MAX_MONTH_IN_YEAR) {
             System.err.println("More than 12 month working per year");
         }
 
-        if (employee.getSpouse() != null) {
-            tax = (int) Math.round(TAX_RATE * (((employee.getMonthlySalary() + employee.getOtherMonthlyIncome()) * employee.getMonthWorkingInYear()) - employee.getAnnualDeductible() - (NON_TAXABLE_INCOME + NON_TAXABLE_ADDITION_FOR_NON_MARRIED + (numberOfChildren * ADDITION_PER_CHILD))));
-        } else {
-            tax = (int) Math.round(TAX_RATE * (((employee.getMonthlySalary() + employee.getOtherMonthlyIncome()) * employee.getMonthWorkingInYear()) - employee.getAnnualDeductible() - NON_TAXABLE_INCOME));
-        }
+        int totalNetYearlyIncome = ((employee.getMonthlySalary() + employee.getOtherMonthlyIncome()) * employee.getMonthWorkingInYear()) - employee.getAnnualDeductible();
+        int totalNonTaxable = employee.getSpouse() != null ? (NON_TAXABLE_INCOME + NON_TAXABLE_ADDITION_FOR_NON_MARRIED + (numberOfChildren * ADDITION_PER_CHILD)) : NON_TAXABLE_INCOME;
+        
+        int tax = (int) Math.round(TAX_RATE * (totalNetYearlyIncome - totalNonTaxable));
 
         if (tax < 0) return 0;
         return tax;
